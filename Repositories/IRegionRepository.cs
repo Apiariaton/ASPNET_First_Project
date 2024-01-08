0d2801aa10bdd076982668a901e8d81006af8643
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-08 20:19:04 +0000

    Implement a Repository as a means to interact with the database via DbContext, instead of the controller

---
+++ Repositories/IRegionRepository.cs
@@ -0,0 +1,15 @@
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+
+    public interface IRegionRepository
+    {
+        Task<List<Region>> GetAllAsync();
+
+    }
+
+
+
+
+}
