735d782b9774740026fe7ee41c63a60d9e2d9233
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-29 13:55:09 +0000

    Create example User Roles to access API in NZWalksAuthDBContext

--- NZWalksDBContext/NZWalksAuthDBContext.cs
+++ NZWalksDBContext/NZWalksAuthDBContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Controllers;
 using NZWalks.API.Models.Domain;
@@ -12,6 +13,37 @@ namespace NZWalks.API.Data {
         {
 
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var ReaderRoleID = "dbcf2358-cbc4-4aca-a99e-6eafe4447674";
+            var WriterRoleID = "6c6729c0-6561-4c20-b4da-3fed150360d2";
+
+
+
+            var ListOfUserRolesToAccessAPI = new List<IdentityRole>{
+
+                new IdentityRole
+                {
+                    Id = ReaderRoleID,
+                    ConcurrencyStamp = ReaderRoleID,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()   
+                },
+
+                new IdentityRole
+                {
+                    Id = WriterRoleID,
+                    ConcurrencyStamp = WriterRoleID,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(ListOfUserRolesToAccessAPI);
         }
 
 
